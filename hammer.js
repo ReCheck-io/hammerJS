e7e5cfaaddd1ab735b5cb59b20ee5785cf5e8a5b
Desislav Petrov <dpetrov@recheck.io>
2021-11-10 10:48:07 +0200

    update library to 1.0.23.beta.1, set to eth network, minor fixes

--- hammer.js
+++ hammer.js
@@ -14,7 +14,7 @@ const aes256 = require('aes256');
 const btoa = require('btoa');
 const atob = require('atob');
 
-let hammerNetwork = "ae";
+let hammerNetwork = "eth";
 let hammerBaseUrl = "http://localhost:4000";
 const loginDevice = 'hammer-0.2.0';
 
